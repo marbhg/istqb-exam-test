f757922a3b0a2a363038dcf5a76369c2745f0fc5
Marrbe <132088541+marbhg@users.noreply.github.com>
2024-12-20 21:55:10 +0100

    atc
    sessi

--- script.js
+++ script.js
@@ -2,9 +2,12 @@ function mostrarDatosdelFormulario() {
   let nombre = document.getElementById("nombre").value;
 
   let apellido = document.getElementById("apellido").value;
-
+  
+//Mostrar nombre y apellido en mayuscula.
   document.getElementById("bienvenido").textContent = " Bienvenido " + nombre.toUpperCase() + " " + apellido.toUpperCase();
 
   //Ocultas el formulario despues de enviarlo 
   document.getElementById("formularioestudiante").style.display = "none"; 
+
+
 }
