ab9e5c8dd0f0acbc32bb6425cf463476e3f39f04
Marrbe <132088541+marbhg@users.noreply.github.com>
2024-12-21 17:25:47 +0100

    ACT
    Question

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function mostrarDatosdelFormulario() {
   document.getElementById("formularioestudiante").style.display = "none"; 
 
   //Mostrar datos de examen STQL
-// Función para verificar la respuesta seleccionada
+// Función para verificar la respuesta seleccionada QUESTION1
 function checkAnswer(questionNumber) {
   const messageElement = document.getElementById(`grademessage${questionNumber}`);
   const correctAnswer = "c"; // La respuesta correcta para esta pregunta
@@ -35,5 +35,216 @@ function resetGradeMessage(questionNumber) {
   messageElement.textContent = "";
 }
 
+// Función para verificar la respuesta seleccionada QUESTION2
+function checkAnswer(questionNumber) {
+  const messageElement = document.getElementById(`grademessage${questionNumber}`);
+  const correctAnswer = "a"; // La respuesta correcta para esta pregunta
+  const selectedOption = document.querySelector(`.question${questionNumber} input[name="answer"]:checked`);
+
+  if (!selectedOption) {
+      messageElement.textContent = "Please select an answer.";
+      messageElement.style.color = "orange";
+  } else if (selectedOption.value === correctAnswer) {
+      messageElement.textContent = "Correct! This statement is achievable.";
+      messageElement.style.color = "green";
+  } else {
+      messageElement.textContent = "Incorrect. Please try again.";
+      messageElement.style.color = "red";
+  }
+}
+
+// Función para reiniciar el mensaje de respuesta
+function resetGradeMessage(questionNumber) {
+  const messageElement = document.getElementById(`grademessage${questionNumber}`);
+  messageElement.textContent = "";
+}
+
+// Función para verificar la respuesta seleccionada QUESTION3
+function checkAnswer(questionNumber) {
+  const messageElement = document.getElementById(`grademessage${questionNumber}`);
+  const correctAnswer = "b"; // La respuesta correcta para esta pregunta
+  const selectedOption = document.querySelector(`.question${questionNumber} input[name="answer"]:checked`);
+
+  if (!selectedOption) {
+      messageElement.textContent = "Please select an answer.";
+      messageElement.style.color = "orange";
+  } else if (selectedOption.value === correctAnswer) {
+      messageElement.textContent = "Correct! This statement is achievable.";
+      messageElement.style.color = "green";
+  } else {
+      messageElement.textContent = "Incorrect. Please try again.";
+      messageElement.style.color = "red";
+  }
+}
+// Función para reiniciar el mensaje de respuestA
+function resetGradeMessage(questionNumber) {
+  const messageElement = document.getElementById(`grademessage${questionNumber}`);
+  messageElement.textContent = "";
+}
+// Función para verificar la respuesta seleccionada QUESTION4
+function checkAnswer(questionNumber) {
+  const messageElement = document.getElementById(`grademessage${questionNumber}`);
+  const correctAnswer = "b"; // La respuesta correcta para esta pregunta
+  const selectedOption = document.querySelector(`.question${questionNumber} input[name="answer"]:checked`);
+
+  if (!selectedOption) {
+      messageElement.textContent = "Please select an answer.";
+      messageElement.style.color = "orange";
+  } else if (selectedOption.value === correctAnswer) {
+      messageElement.textContent = "Correct! This statement is achievable.";
+      messageElement.style.color = "green";
+  } else {
+      messageElement.textContent = "Incorrect. Please try again.";
+      messageElement.style.color = "red";
+  }
+}
+
+// Función para reiniciar el mensaje de respuesta
+function resetGradeMessage(questionNumber) {
+  const messageElement = document.getElementById(`grademessage${questionNumber}`);
+  messageElement.textContent = "";
+}
+
+// Función para verificar la respuesta seleccionada QUESTION5
+function checkAnswer(questionNumber) {
+  const messageElement = document.getElementById(`grademessage${questionNumber}`);
+  const correctAnswer = "b"; // La respuesta correcta para esta pregunta
+  const selectedOption = document.querySelector(`.question${questionNumber} input[name="answer"]:checked`);
+
+  if (!selectedOption) {
+      messageElement.textContent = "Please select an answer.";
+      messageElement.style.color = "orange";
+  } else if (selectedOption.value === correctAnswer) {
+      messageElement.textContent = "Correct! This statement is achievable.";
+      messageElement.style.color = "green";
+  } else {
+      messageElement.textContent = "Incorrect. Please try again.";
+      messageElement.style.color = "red";
+  }
+}
+
+// Función para reiniciar el mensaje de respuesta
+function resetGradeMessage(questionNumber) {
+  const messageElement = document.getElementById(`grademessage${questionNumber}`);
+  messageElement.textContent = "";
+}
+// Función para verificar la respuesta seleccionada QUESTION6
+function checkAnswer(questionNumber) {
+  const messageElement = document.getElementById(`grademessage${questionNumber}`);
+  const correctAnswer = "a"; // La respuesta correcta para esta pregunta
+  const selectedOption = document.querySelector(`.question${questionNumber} input[name="answer"]:checked`);
+
+  if (!selectedOption) {
+      messageElement.textContent = "Please select an answer.";
+      messageElement.style.color = "orange";
+  } else if (selectedOption.value === correctAnswer) {
+      messageElement.textContent = "Correct! This statement is achievable.";
+      messageElement.style.color = "green";
+  } else {
+      messageElement.textContent = "Incorrect. Please try again.";
+      messageElement.style.color = "red";
+  }
+}
+
+// Función para reiniciar el mensaje de respuesta
+function resetGradeMessage(questionNumber) {
+  const messageElement = document.getElementById(`grademessage${questionNumber}`);
+  messageElement.textContent = "";
+}
+// Función para verificar la respuesta seleccionada QUESTION7
+function checkAnswer(questionNumber) {
+  const messageElement = document.getElementById(`grademessage${questionNumber}`);
+  const correctAnswer = "a"; // La respuesta correcta para esta pregunta
+  const selectedOption = document.querySelector(`.question${questionNumber} input[name="answer"]:checked`);
+
+  if (!selectedOption) {
+      messageElement.textContent = "Please select an answer.";
+      messageElement.style.color = "orange";
+  } else if (selectedOption.value === correctAnswer) {
+      messageElement.textContent = "Correct! This statement is achievable.";
+      messageElement.style.color = "green";
+  } else {
+      messageElement.textContent = "Incorrect. Please try again.";
+      messageElement.style.color = "red";
+  }
+}
+
+// Función para reiniciar el mensaje de respuesta
+function resetGradeMessage(questionNumber) {
+  const messageElement = document.getElementById(`grademessage${questionNumber}`);
+  messageElement.textContent = "";
+}
+
+// Función para verificar la respuesta seleccionada QUESTION8
+function checkAnswer(questionNumber) {
+  const messageElement = document.getElementById(`grademessage${questionNumber}`);
+  const correctAnswer = "c"; // La respuesta correcta para esta pregunta
+  const selectedOption = document.querySelector(`.question${questionNumber} input[name="answer"]:checked`);
+
+  if (!selectedOption) {
+      messageElement.textContent = "Please select an answer.";
+      messageElement.style.color = "orange";
+  } else if (selectedOption.value === correctAnswer) {
+      messageElement.textContent = "Correct! This statement is achievable.";
+      messageElement.style.color = "green";
+  } else {
+      messageElement.textContent = "Incorrect. Please try again.";
+      messageElement.style.color = "red";
+  }
+}
+
+// Función para reiniciar el mensaje de respuesta
+function resetGradeMessage(questionNumber) {
+  const messageElement = document.getElementById(`grademessage${questionNumber}`);
+  messageElement.textContent = "";
+}
+
+// Función para verificar la respuesta seleccionada QUESTION9
+function checkAnswer(questionNumber) {
+  const messageElement = document.getElementById(`grademessage${questionNumber}`);
+  const correctAnswer = "a"; // La respuesta correcta para esta pregunta
+  const selectedOption = document.querySelector(`.question${questionNumber} input[name="answer"]:checked`);
+
+  if (!selectedOption) {
+      messageElement.textContent = "Please select an answer.";
+      messageElement.style.color = "orange";
+  } else if (selectedOption.value === correctAnswer) {
+      messageElement.textContent = "Correct! This statement is achievable.";
+      messageElement.style.color = "green";
+  } else {
+      messageElement.textContent = "Incorrect. Please try again.";
+      messageElement.style.color = "red";
+  }
+}
+
+// Función para reiniciar el mensaje de respuesta
+function resetGradeMessage(questionNumber) {
+  const messageElement = document.getElementById(`grademessage${questionNumber}`);
+  messageElement.textContent = "";
+}
+
+// Función para verificar la respuesta seleccionada QUESTION10
+function checkAnswer(questionNumber) {
+  const messageElement = document.getElementById(`grademessage${questionNumber}`);
+  const correctAnswer = "c"; // La respuesta correcta para esta pregunta
+  const selectedOption = document.querySelector(`.question${questionNumber} input[name="answer"]:checked`);
+
+  if (!selectedOption) {
+      messageElement.textContent = "Please select an answer.";
+      messageElement.style.color = "orange";
+  } else if (selectedOption.value === correctAnswer) {
+      messageElement.textContent = "Correct! This statement is achievable.";
+      messageElement.style.color = "green";
+  } else {
+      messageElement.textContent = "Incorrect. Please try again.";
+      messageElement.style.color = "red";
+  }
+}
+
+// Función para reiniciar el mensaje de respuesta
+function resetGradeMessage(questionNumber) {
+  const messageElement = document.getElementById(`grademessage${questionNumber}`);
+  messageElement.textContent = "";
+}
 }.
  
