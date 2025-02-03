c051ae423851f5291d930b6f5877aa99b7e1deb1
Marrbe <132088541+marbhg@users.noreply.github.com>
2025-02-03 16:05:35 +0100

    Actualizo
    question

--- script.js
+++ script.js
@@ -14,58 +14,58 @@ function enviar() {
 }
 
 function validarFormulario() {
-let formulario = document.getElementById("signupform");
+  let formulario = document.getElementById("signupform");
 
-if (formulario.checkValidity()){
-  enviar()
-}
+  if (formulario.checkValidity()) {
+    enviar()
+  }
   else {
     document.getElementById("datosdelformulario").textContent = " Invalid form ";
   }
 }
 //Calculamos la puntuacion final de todo el examen
-function evaluarTodo(){
+function evaluarTodo() {
   let puntuacionFinal = 0;
-  for(let numeroPregunta = 1; numeroPregunta<=10; numeroPregunta++){
-  puntuacionFinal += calcularPuntuacionPregunta(numeroPregunta);
+  for (let numeroPregunta = 1; numeroPregunta <= 10; numeroPregunta++) {
+    puntuacionFinal += calcularPuntuacionPregunta(numeroPregunta);
   }
 
-if (puntuacionFinal < 0){
-  puntuacionFinal = 0;
-}
+  if (puntuacionFinal < 0) {
+    puntuacionFinal = 0;
+  }
 
   document.getElementById('gradeexammessage').textContent = "La puntuacion final es " + puntuacionFinal;
   document.getElementById('gradeexammessage').style.color = colorResultado(puntuacionFinal)
 }
 
 //Hacemos una funcion para calcular todas las preguntas. 
-  function calcularPuntuacionPregunta(numeroPregunta) {
-    let respuesta = document.getElementById("respuesta" + numeroPregunta).value; // Accede al valor del botón seleccionado
-    let puntuacion = 0;
-    let correcta = ["c","a","b","a","a","c","b","a","a","c"];
+function calcularPuntuacionPregunta(numeroPregunta) {
+  let respuesta = document.getElementById("respuesta" + numeroPregunta).value; // Accede al valor del botón seleccionado
+  let puntuacion = 0;
+  let correcta = ["c", "a", "b", "a", "a", "c", "b", "a", "a", "c"];
 
-    // Calcular la puntuación según las reglas
-    if (respuesta === correcta[numeroPregunta-1]) {
-      puntuacion = 2; // Respuesta correcta
-    } else if (respuesta === "") {
-      puntuacion = 0; // Respuesta vacia
-    } else {
-      puntuacion = -1; // Incorrecta
-    }
-
-    document.getElementById("grademessage" +  numeroPregunta).textContent = "El resultado de la pregunta es " + puntuacion; 
-     return puntuacion;
-
+  // Calcular la puntuación según las reglas
+  if (respuesta === correcta[numeroPregunta - 1]) {
+    puntuacion = 2; // Respuesta correcta
+  } else if (respuesta === "") {
+    puntuacion = 0; // Respuesta vacia
+  } else {
+    puntuacion = -1; // Incorrecta
   }
 
+  document.getElementById("grademessage" + numeroPregunta).textContent = "El resultado de la pregunta es " + puntuacion;
+  return puntuacion;
 
-  function colorResultado(correcta) {
-    // devolvemos el color que debe mostrar segun la puntuacion obtenida
-    let color = 'green';
-    if (correcta > 12) {
-        color = 'green';
-    } else {
-        color = 'red';
+}
+
+
+function colorResultado(correcta) {
+  // devolvemos el color que debe mostrar segun la puntuacion obtenida
+  let color = 'green';
+  if (correcta > 12) {
+    color = 'green';
+  } else {
+    color = 'red';
   }
-     return color;
+  return color;
 }
